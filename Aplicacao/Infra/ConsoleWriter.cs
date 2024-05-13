1d9ce9e52663f5a237a561af81ab034148e6fd4f
Rogério Santos <rogersanctus@gmail.com>
2024-05-13 12:55:20 -0300

    Add an util to write colored text to console

---
+++ Aplicacao/Infra/ConsoleWriter.cs
@@ -0,0 +1,20 @@
+namespace DesafioDioEstacionamento.Infra.Utils;
+
+public static class ConsoleWriter
+{
+  public static void Write(string message, ConsoleColor color = ConsoleColor.White)
+  {
+    Console.ForegroundColor = color;
+    Console.Write(message);
+    Console.ResetColor();
+  }
+
+  public static void WriteLine(string message, ConsoleColor color = ConsoleColor.White)
+  {
+    Console.ForegroundColor = color;
+    Console.WriteLine(message);
+    Console.ResetColor();
+  }
+
+  public static void WriteLine() => Console.WriteLine();
+}
